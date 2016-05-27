1b7ba7d751a32640134716f407ff0a156225b73f
Yousong Zhou <yszhou4tech@gmail.com>
2016-05-27 10:48:08 +0800

    recovery-kernel.bin: truncated part

---
+++ MX3.Flyme.OS.4.5.7.1A.cn/recovery-kernel.bin.asm
@@ -0,0 +1,356 @@
+c00145ac:	e1a0c00d 	mov	ip, sp
+c00145b0:	e92dddf0 	push	{r4, r5, r6, r7, r8, sl, fp, ip, lr, pc}
+c00145b4:	e24cb004 	sub	fp, ip, #4
+c00145b8:	e24dd088 	sub	sp, sp, #136	; 0x88
+c00145bc:	e1a02840 	asr	r2, r0, #16
+c00145c0:	e59f554c 	ldr	r5, [pc, #1356]	; 0xc0014b14
+c00145c4:	e352000f 	cmp	r2, #15
+c00145c8:	e1a03000 	mov	r3, r0
+c00145cc:	e1a06001 	mov	r6, r1
+c00145d0:	e08f5005 	add	r5, pc, r5
+c00145d4:	e1a0200d 	mov	r2, sp
+c00145d8:	1a00006d 	bne	0xc0014794
+c00145dc:	e6ff1070 	uxth	r1, r0
+c00145e0:	e3510069 	cmp	r1, #105	; 0x69
+c00145e4:	908ff101 	addls	pc, pc, r1, lsl #2
+c00145e8:	ea00012e 	b	0xc0014aa8
+c00145ec:	ea00011d 	b	0xc0014a68
+c00145f0:	ea00010d 	b	0xc0014a2c
+c00145f4:	ea0000e1 	b	0xc0014980
+c00145f8:	ea0000d6 	b	0xc0014958
+c00145fc:	ea0000bb 	b	0xc00148f0
+c0014600:	ea0000b3 	b	0xc00148d4
+c0014604:	ea000127 	b	0xc0014aa8
+c0014608:	ea000126 	b	0xc0014aa8
+c001460c:	ea000125 	b	0xc0014aa8
+c0014610:	ea000124 	b	0xc0014aa8
+c0014614:	ea000123 	b	0xc0014aa8
+c0014618:	ea000122 	b	0xc0014aa8
+c001461c:	ea000121 	b	0xc0014aa8
+c0014620:	ea000120 	b	0xc0014aa8
+c0014624:	ea00011f 	b	0xc0014aa8
+c0014628:	ea00011e 	b	0xc0014aa8
+c001462c:	ea00011d 	b	0xc0014aa8
+c0014630:	ea00011c 	b	0xc0014aa8
+c0014634:	ea00011b 	b	0xc0014aa8
+c0014638:	ea00011a 	b	0xc0014aa8
+c001463c:	ea000119 	b	0xc0014aa8
+c0014640:	ea000118 	b	0xc0014aa8
+c0014644:	ea000117 	b	0xc0014aa8
+c0014648:	ea000116 	b	0xc0014aa8
+c001464c:	ea000115 	b	0xc0014aa8
+c0014650:	ea000114 	b	0xc0014aa8
+c0014654:	ea000113 	b	0xc0014aa8
+c0014658:	ea000112 	b	0xc0014aa8
+c001465c:	ea000111 	b	0xc0014aa8
+c0014660:	ea000110 	b	0xc0014aa8
+c0014664:	ea00010f 	b	0xc0014aa8
+c0014668:	ea00010e 	b	0xc0014aa8
+c001466c:	ea00010d 	b	0xc0014aa8
+c0014670:	ea00010c 	b	0xc0014aa8
+c0014674:	ea00010b 	b	0xc0014aa8
+c0014678:	ea00010a 	b	0xc0014aa8
+c001467c:	ea000109 	b	0xc0014aa8
+c0014680:	ea000108 	b	0xc0014aa8
+c0014684:	ea000107 	b	0xc0014aa8
+c0014688:	ea000106 	b	0xc0014aa8
+c001468c:	ea000105 	b	0xc0014aa8
+c0014690:	ea000104 	b	0xc0014aa8
+c0014694:	ea000103 	b	0xc0014aa8
+c0014698:	ea000102 	b	0xc0014aa8
+c001469c:	ea000101 	b	0xc0014aa8
+c00146a0:	ea000100 	b	0xc0014aa8
+c00146a4:	ea0000ff 	b	0xc0014aa8
+c00146a8:	ea0000fe 	b	0xc0014aa8
+c00146ac:	ea0000fd 	b	0xc0014aa8
+c00146b0:	ea0000fc 	b	0xc0014aa8
+c00146b4:	ea0000fb 	b	0xc0014aa8
+c00146b8:	ea0000fa 	b	0xc0014aa8
+c00146bc:	ea0000f9 	b	0xc0014aa8
+c00146c0:	ea0000f8 	b	0xc0014aa8
+c00146c4:	ea0000f7 	b	0xc0014aa8
+c00146c8:	ea0000f6 	b	0xc0014aa8
+c00146cc:	ea0000f5 	b	0xc0014aa8
+c00146d0:	ea0000f4 	b	0xc0014aa8
+c00146d4:	ea0000f3 	b	0xc0014aa8
+c00146d8:	ea0000f2 	b	0xc0014aa8
+c00146dc:	ea0000f1 	b	0xc0014aa8
+c00146e0:	ea0000f0 	b	0xc0014aa8
+c00146e4:	ea0000ef 	b	0xc0014aa8
+c00146e8:	ea0000ee 	b	0xc0014aa8
+c00146ec:	ea0000ed 	b	0xc0014aa8
+c00146f0:	ea0000ec 	b	0xc0014aa8
+c00146f4:	ea0000eb 	b	0xc0014aa8
+c00146f8:	ea0000ea 	b	0xc0014aa8
+c00146fc:	ea0000e9 	b	0xc0014aa8
+c0014700:	ea0000e8 	b	0xc0014aa8
+c0014704:	ea0000e7 	b	0xc0014aa8
+c0014708:	ea0000e6 	b	0xc0014aa8
+c001470c:	ea0000e5 	b	0xc0014aa8
+c0014710:	ea0000e4 	b	0xc0014aa8
+c0014714:	ea0000e3 	b	0xc0014aa8
+c0014718:	ea0000e2 	b	0xc0014aa8
+c001471c:	ea0000e1 	b	0xc0014aa8
+c0014720:	ea0000e0 	b	0xc0014aa8
+c0014724:	ea0000df 	b	0xc0014aa8
+c0014728:	ea0000de 	b	0xc0014aa8
+c001472c:	ea0000dd 	b	0xc0014aa8
+c0014730:	ea0000dc 	b	0xc0014aa8
+c0014734:	ea0000db 	b	0xc0014aa8
+c0014738:	ea0000da 	b	0xc0014aa8
+c001473c:	ea0000d9 	b	0xc0014aa8
+c0014740:	ea0000d8 	b	0xc0014aa8
+c0014744:	ea0000d7 	b	0xc0014aa8
+c0014748:	ea0000d6 	b	0xc0014aa8
+c001474c:	ea0000d5 	b	0xc0014aa8
+c0014750:	ea0000d4 	b	0xc0014aa8
+c0014754:	ea0000d3 	b	0xc0014aa8
+c0014758:	ea0000d2 	b	0xc0014aa8
+c001475c:	ea0000d1 	b	0xc0014aa8
+c0014760:	ea0000d0 	b	0xc0014aa8
+c0014764:	ea0000cf 	b	0xc0014aa8
+c0014768:	ea0000ce 	b	0xc0014aa8
+c001476c:	ea0000cd 	b	0xc0014aa8
+c0014770:	ea0000cc 	b	0xc0014aa8
+c0014774:	ea0000cb 	b	0xc0014aa8
+c0014778:	ea0000ca 	b	0xc0014aa8
+c001477c:	ea0000c9 	b	0xc0014aa8
+c0014780:	ea00004f 	b	0xc00148c4
+c0014784:	ea00003b 	b	0xc0014878
+c0014788:	ea000020 	b	0xc0014810
+c001478c:	ea000009 	b	0xc00147b8
+c0014790:	ea000004 	b	0xc00147a8
+c0014794:	ebfffedd 	bl	0xc0014310
+c0014798:	e1a04000 	mov	r4, r0
+c001479c:	e1a00004 	mov	r0, r4
+c00147a0:	e24bd024 	sub	sp, fp, #36	; 0x24
+c00147a4:	e89dadf0 	ldm	sp, {r4, r5, r6, r7, r8, sl, fp, sp, pc}
+c00147a8:	e5960000 	ldr	r0, [r6]
+c00147ac:	ebfff9b5 	bl	0xc0012e88
+c00147b0:	e1a04000 	mov	r4, r0
+c00147b4:	eafffff8 	b	0xc001479c
+c00147b8:	e59f0358 	ldr	r0, [pc, #856]	; 0xc0014b18
+c00147bc:	e5965000 	ldr	r5, [r6]
+c00147c0:	e08f0000 	add	r0, pc, r0
+c00147c4:	e5964008 	ldr	r4, [r6, #8]
+c00147c8:	e2800008 	add	r0, r0, #8
+c00147cc:	e5967004 	ldr	r7, [r6, #4]
+c00147d0:	eb0a5d47 	bl	0xc02abcf4
+c00147d4:	e3550001 	cmp	r5, #1
+c00147d8:	83e04015 	mvnhi	r4, #21
+c00147dc:	8a000006 	bhi	0xc00147fc
+c00147e0:	e1a02004 	mov	r2, r4
+c00147e4:	e1a00005 	mov	r0, r5
+c00147e8:	e1a01007 	mov	r1, r7
+c00147ec:	eb003e34 	bl	0xc00240c4
+c00147f0:	e3500000 	cmp	r0, #0
+c00147f4:	01a04000 	moveq	r4, r0
+c00147f8:	13e0400d 	mvnne	r4, #13
+c00147fc:	e59f0318 	ldr	r0, [pc, #792]	; 0xc0014b1c
+c0014800:	e08f0000 	add	r0, pc, r0
+c0014804:	e2800008 	add	r0, r0, #8
+c0014808:	eb0a5c7b 	bl	0xc02ab9fc
+c001480c:	eaffffe2 	b	0xc001479c
+c0014810:	e59f0308 	ldr	r0, [pc, #776]	; 0xc0014b20
+c0014814:	e5964004 	ldr	r4, [r6, #4]
+c0014818:	e08f0000 	add	r0, pc, r0
+c001481c:	e5965000 	ldr	r5, [r6]
+c0014820:	e2800008 	add	r0, r0, #8
+c0014824:	e5967008 	ldr	r7, [r6, #8]
+c0014828:	e596600c 	ldr	r6, [r6, #12]
+c001482c:	eb0a5d30 	bl	0xc02abcf4
+c0014830:	e3540001 	cmp	r4, #1
+c0014834:	0a0000ab 	beq	0xc0014ae8
+c0014838:	e3540002 	cmp	r4, #2
+c001483c:	0a0000a2 	beq	0xc0014acc
+c0014840:	e3540000 	cmp	r4, #0
+c0014844:	13e04015 	mvnne	r4, #21
+c0014848:	1a000005 	bne	0xc0014864
+c001484c:	e1a00005 	mov	r0, r5
+c0014850:	e1a01006 	mov	r1, r6
+c0014854:	eb003d85 	bl	0xc0023e70
+c0014858:	e3500000 	cmp	r0, #0
+c001485c:	01a04000 	moveq	r4, r0
+c0014860:	13e0400d 	mvnne	r4, #13
+c0014864:	e59f02b8 	ldr	r0, [pc, #696]	; 0xc0014b24
+c0014868:	e08f0000 	add	r0, pc, r0
+c001486c:	e2800008 	add	r0, r0, #8
+c0014870:	eb0a5c61 	bl	0xc02ab9fc
+c0014874:	eaffffc8 	b	0xc001479c
+c0014878:	e1a0200d 	mov	r2, sp
+c001487c:	e3c23d7f 	bic	r3, r2, #8128	; 0x1fc0
+c0014880:	e3c3303f 	bic	r3, r3, #63	; 0x3f
+c0014884:	e5960000 	ldr	r0, [r6]
+c0014888:	e5933008 	ldr	r3, [r3, #8]
+c001488c:	e2902b01 	adds	r2, r0, #1024	; 0x400
+c0014890:	30d22003 	sbcscc	r2, r2, r3
+c0014894:	33a03000 	movcc	r3, #0
+c0014898:	e3530000 	cmp	r3, #0
+c001489c:	13e0400d 	mvnne	r4, #13
+c00148a0:	1affffbd 	bne	0xc001479c
+c00148a4:	e59f127c 	ldr	r1, [pc, #636]	; 0xc0014b28
+c00148a8:	e3a02b01 	mov	r2, #1024	; 0x400
+c00148ac:	e08f1001 	add	r1, pc, r1
+c00148b0:	eb050262 	bl	0xc0155240
+c00148b4:	e3500000 	cmp	r0, #0
+c00148b8:	01a04000 	moveq	r4, r0
+c00148bc:	13e0400d 	mvnne	r4, #13
+c00148c0:	eaffffb5 	b	0xc001479c
+c00148c4:	e5960000 	ldr	r0, [r6]
+c00148c8:	ebfffcbc 	bl	0xc0013bc0
+c00148cc:	e1a04000 	mov	r4, r0
+c00148d0:	eaffffb1 	b	0xc001479c
+c00148d4:	e5963000 	ldr	r3, [r6]
+c00148d8:	e3c22d7f 	bic	r2, r2, #8128	; 0x1fc0
+c00148dc:	e3c2203f 	bic	r2, r2, #63	; 0x3f
+c00148e0:	e5823060 	str	r3, [r2, #96]	; 0x60
+c00148e4:	ee0d3f70 	mcr	15, 0, r3, cr13, cr0, {3}
+c00148e8:	e3a04000 	mov	r4, #0
+c00148ec:	eaffffaa 	b	0xc001479c
+c00148f0:	e59f2234 	ldr	r2, [pc, #564]	; 0xc0014b2c
+c00148f4:	e7952002 	ldr	r2, [r5, r2]
+c00148f8:	e5922000 	ldr	r2, [r2]
+c00148fc:	e3120008 	tst	r2, #8
+c0014900:	1a00006c 	bne	0xc0014ab8
+c0014904:	e5962040 	ldr	r2, [r6, #64]	; 0x40
+c0014908:	e3a0c000 	mov	ip, #0
+c001490c:	e59f021c 	ldr	r0, [pc, #540]	; 0xc0014b30
+c0014910:	e1a01006 	mov	r1, r6
+c0014914:	e596403c 	ldr	r4, [r6, #60]	; 0x3c
+c0014918:	e3120020 	tst	r2, #32
+c001491c:	e08f0000 	add	r0, pc, r0
+c0014920:	e24b20a4 	sub	r2, fp, #164	; 0xa4
+c0014924:	03a0e004 	moveq	lr, #4
+c0014928:	13a0e002 	movne	lr, #2
+c001492c:	e50bc0a0 	str	ip, [fp, #-160]	; 0xffffff60
+c0014930:	e06e4004 	rsb	r4, lr, r4
+c0014934:	e58dc000 	str	ip, [sp]
+c0014938:	e3a0e004 	mov	lr, #4
+c001493c:	e50b4098 	str	r4, [fp, #-152]	; 0xffffff68
+c0014940:	e50be0a4 	str	lr, [fp, #-164]	; 0xffffff5c
+c0014944:	e1a0400c 	mov	r4, ip
+c0014948:	e340e003 	movt	lr, #3
+c001494c:	e50be09c 	str	lr, [fp, #-156]	; 0xffffff64
+c0014950:	ebfffe58 	bl	0xc00142b8
+c0014954:	eaffff90 	b	0xc001479c
+c0014958:	e59f21cc 	ldr	r2, [pc, #460]	; 0xc0014b2c
+c001495c:	e7952002 	ldr	r2, [r5, r2]
+c0014960:	e5922000 	ldr	r2, [r2]
+c0014964:	e3120008 	tst	r2, #8
+c0014968:	0affffe5 	beq	0xc0014904
+c001496c:	e5963040 	ldr	r3, [r6, #64]	; 0x40
+c0014970:	e5964000 	ldr	r4, [r6]
+c0014974:	e3c33010 	bic	r3, r3, #16
+c0014978:	e5863040 	str	r3, [r6, #64]	; 0x40
+c001497c:	eaffff86 	b	0xc001479c
+c0014980:	e1a0200d 	mov	r2, sp
+c0014984:	e3c23d7f 	bic	r3, r2, #8128	; 0x1fc0
+c0014988:	e3c3303f 	bic	r3, r3, #63	; 0x3f
+c001498c:	e5964008 	ldr	r4, [r6, #8]
+c0014990:	e5967000 	ldr	r7, [r6]
+c0014994:	e5968004 	ldr	r8, [r6, #4]
+c0014998:	e593300c 	ldr	r3, [r3, #12]
+c001499c:	e1570008 	cmp	r7, r8
+c00149a0:	93540000 	cmpls	r4, #0
+c00149a4:	e593a0d8 	ldr	sl, [r3, #216]	; 0xd8
+c00149a8:	03a04000 	moveq	r4, #0
+c00149ac:	13a04001 	movne	r4, #1
+c00149b0:	13a04000 	movne	r4, #0
+c00149b4:	1affff78 	bne	0xc001479c
+c00149b8:	e28a603c 	add	r6, sl, #60	; 0x3c
+c00149bc:	e1a00006 	mov	r0, r6
+c00149c0:	eb0a5db3 	bl	0xc02ac094
+c00149c4:	e1a0000a 	mov	r0, sl
+c00149c8:	e1a01007 	mov	r1, r7
+c00149cc:	eb020a24 	bl	0xc0097264
+c00149d0:	e3500000 	cmp	r0, #0
+c00149d4:	0a00004a 	beq	0xc0014b04
+c00149d8:	e5903004 	ldr	r3, [r0, #4]
+c00149dc:	e1580003 	cmp	r8, r3
+c00149e0:	9a000047 	bls	0xc0014b04
+c00149e4:	e5902008 	ldr	r2, [r0, #8]
+c00149e8:	e1a00006 	mov	r0, r6
+c00149ec:	e1530007 	cmp	r3, r7
+c00149f0:	21a07003 	movcs	r7, r3
+c00149f4:	e1580002 	cmp	r8, r2
+c00149f8:	21a08002 	movcs	r8, r2
+c00149fc:	eb00dbe2 	bl	0xc004b98c
+c0014a00:	e59f312c 	ldr	r3, [pc, #300]	; 0xc0014b34
+c0014a04:	e2881eff 	add	r1, r8, #4080	; 0xff0
+c0014a08:	e281100f 	add	r1, r1, #15
+c0014a0c:	e3c70eff 	bic	r0, r7, #4080	; 0xff0
+c0014a10:	e3c11eff 	bic	r1, r1, #4080	; 0xff0
+c0014a14:	e3c0000f 	bic	r0, r0, #15
+c0014a18:	e7953003 	ldr	r3, [r5, r3]
+c0014a1c:	e3c1100f 	bic	r1, r1, #15
+c0014a20:	e5933018 	ldr	r3, [r3, #24]
+c0014a24:	e12fff33 	blx	r3
+c0014a28:	eaffff5b 	b	0xc001479c
+c0014a2c:	e5961040 	ldr	r1, [r6, #64]	; 0x40
+c0014a30:	e1a0200d 	mov	r2, sp
+c0014a34:	e3c23d7f 	bic	r3, r2, #8128	; 0x1fc0
+c0014a38:	e596203c 	ldr	r2, [r6, #60]	; 0x3c
+c0014a3c:	e3110020 	tst	r1, #32
+c0014a40:	e3c3303f 	bic	r3, r3, #63	; 0x3f
+c0014a44:	e1a01006 	mov	r1, r6
+c0014a48:	e593000c 	ldr	r0, [r3, #12]
+c0014a4c:	03a03004 	moveq	r3, #4
+c0014a50:	13a03002 	movne	r3, #2
+c0014a54:	e0633002 	rsb	r3, r3, r2
+c0014a58:	e586303c 	str	r3, [r6, #60]	; 0x3c
+c0014a5c:	ebfff1c4 	bl	0xc0011174
+c0014a60:	e5964000 	ldr	r4, [r6]
+c0014a64:	eaffff4c 	b	0xc001479c
+c0014a68:	e59f00c8 	ldr	r0, [pc, #200]	; 0xc0014b38
+c0014a6c:	e3a04000 	mov	r4, #0
+c0014a70:	e1a01006 	mov	r1, r6
+c0014a74:	e24b20a4 	sub	r2, fp, #164	; 0xa4
+c0014a78:	e08f0000 	add	r0, pc, r0
+c0014a7c:	e1a03004 	mov	r3, r4
+c0014a80:	e3a0c00b 	mov	ip, #11
+c0014a84:	e50b40a0 	str	r4, [fp, #-160]	; 0xffffff60
+c0014a88:	e50bc0a4 	str	ip, [fp, #-164]	; 0xffffff5c
+c0014a8c:	e3a0c001 	mov	ip, #1
+c0014a90:	e50b4098 	str	r4, [fp, #-152]	; 0xffffff68
+c0014a94:	e340c003 	movt	ip, #3
+c0014a98:	e58d4000 	str	r4, [sp]
+c0014a9c:	e50bc09c 	str	ip, [fp, #-156]	; 0xffffff64
+c0014aa0:	ebfffe04 	bl	0xc00142b8
+c0014aa4:	eaffff3c 	b	0xc001479c
+c0014aa8:	e3510b02 	cmp	r1, #2048	; 0x800
+c0014aac:	b3e04025 	mvnlt	r4, #37	; 0x25
+c0014ab0:	aaffff93 	bge	0xc0014904
+c0014ab4:	eaffff38 	b	0xc001479c
+c0014ab8:	e5963040 	ldr	r3, [r6, #64]	; 0x40
+c0014abc:	e5964000 	ldr	r4, [r6]
+c0014ac0:	e3833010 	orr	r3, r3, #16
+c0014ac4:	e5863040 	str	r3, [r6, #64]	; 0x40
+c0014ac8:	eaffff33 	b	0xc001479c
+c0014acc:	e1a00005 	mov	r0, r5
+c0014ad0:	e1a01007 	mov	r1, r7
+c0014ad4:	eb003d31 	bl	0xc0023fa0
+c0014ad8:	e3500000 	cmp	r0, #0
+c0014adc:	01a04000 	moveq	r4, r0
+c0014ae0:	13e0400d 	mvnne	r4, #13
+c0014ae4:	eaffff5e 	b	0xc0014864
+c0014ae8:	e1a00005 	mov	r0, r5
+c0014aec:	e1a01006 	mov	r1, r6
+c0014af0:	eb003d0f 	bl	0xc0023f34
+c0014af4:	e3500000 	cmp	r0, #0
+c0014af8:	01a04000 	moveq	r4, r0
+c0014afc:	13e0400d 	mvnne	r4, #13
+c0014b00:	eaffff57 	b	0xc0014864
+c0014b04:	e1a00006 	mov	r0, r6
+c0014b08:	e3a04000 	mov	r4, #0
+c0014b0c:	eb00db9e 	bl	0xc004b98c
+c0014b10:	eaffff21 	b	0xc001479c
+c0014b14:	ffff4980 			; <UNDEFINED> instruction: 0xffff4980
+c0014b18:	00450930 	subeq	r0, r5, r0, lsr r9
+c0014b1c:	004508f0 	strdeq	r0, [r5], #-128	; 0xffffff80
+c0014b20:	004508d8 	ldrdeq	r0, [r5], #-136	; 0xffffff78
+c0014b24:	00450888 	subeq	r0, r5, r8, lsl #17
+c0014b28:	003f1b24 	eorseq	r1, pc, r4, lsr #22
+c0014b2c:	fffff33c 			; <UNDEFINED> instruction: 0xfffff33c
+c0014b30:	0035e298 	mlaseq	r5, r8, r2, lr
+c0014b34:	fffff358 			; <UNDEFINED> instruction: 0xfffff358
+c0014b38:	0035e128 	eorseq	lr, r5, r8, lsr #2
---
+++ MX3.Flyme.OS.4.5.7.1A.cn/recovery-kernel.bin.s
@@ -0,0 +1,59 @@
+ 36aa1c <0>Internal error: %s: %x [#%d] PREEMPT SMP ARM
+ 36aa50 <0>Process %.*s (pid: %d, stack limit = 0x%p)
+ 36aa84 Stack: 
+ 36aa8c bad PC value
+ 36aa9c %sCode: %s
+ 36aaa8 Fatal exception in interrupt
+ 36aac8 Fatal exception
+ 36aad8 Oops - bad syscall
+ 36aaec Oops - undefined instruction
+ 36ab0c Hmm.  Unexpected FIQ received, but trying to continue
+ 36ab44 You may have a hardware problem...
+ 36ab68 <2>Bad mode in %s handler detected
+ 36ab8c Oops - bad mode
+ 36ab9c bad mode
+ 36aba8 branch through zero
+ 36abbc Oops - bad syscall(2)
+ 36abd4 xchg: bad data size: pc 0x%p, ptr 0x%p, size %d
+ 36ac08 unknown data abort code
+ 36ac20 %s called, but not implemented
+ 36ac40 %s:%d: bad pte %08llx.
+ 36ac58 %s:%d: bad pmd %08llx.
+ 36ac70 %s:%d: bad pgd %08llx.
+ 36ac88 Division by zero in kernel.
+ 36aca8 prefetch abort
+ 36acb8 data abort
+ 36acc4 address exception
+ 36acd8 interrupt
+ 36ace4 dummy_timer
+ 36acf0 <3>CPU%u: fork() failed
+ 36ad0c <2>CPU%u: failed to come online
+ 36ad30 <3>CPU%u: failed to boot: %d
+ 36ad50 <3>CPU%u: cpu didn't die
+ 36ad6c CPU%u: unable to kill
+ 36ad84 <6>SMP: Total of %d processors activated (%lu.%02lu BogoMIPS).
+ 36adc4 %*s%u: 
+ 36add0 %10u 
+ 3fc5f5 k:Xp
+ 3fc605 y.2c
+ 3fc60a Kl8:
+ 3fc64e bLL6i
+ 3fc6da &zAD
+ 3fc6e1 l,OH
+ 3fc711 ~8%[
+ 3fd2b8 UUUUUUUU
+ 3fd2d8 UUUU
+ 3fd2e0 UUUU
+ 3fd4a8 pfifo_fast
+ 3fd4f4 noop
+ 3fd540 noqueue
+ 3fe3d8 Ty$xCHOrCTZ2ERxW8TV+AflezItpJXE$ND6$Og2!vk+v+HVr~)IAxwR8dY=R_bqwN76uu9K$2cx7$(ZZg~iBALLf0B06P~oWk*3!(xb*CV3zXpLuDxsl99%nUhW#jlA5FrvkrC^j#VM3@qx9C@RM)DP=v@AvCy%V1bKrC^4U9hXPf%owk2MXGzRoyORe&AShnXZhNfq11Np@ztM3zUztI1n7~pIxYaYYRGjieX7l4OweRKyN!U9F$6Cs0aCAZH1!4+t5@NF@yW5EG_0rWdjDd$1+brHw1d36l*4cSbnn1FdwoZnof=wY6XSyO~i8U)aog_wDZ4f7_8u+zG@Gf(AEEQf_SIYmcfSfAflajrkgeRdY^HZyKmOKrMZIUt3XWbyH3t=hrCKty+7z=FKm7DToWvBWPr+Qp*PzZnvw=6^j*S&lD$5OB*b_LCRmo0iw*QDm17&YfuyI+W6qJ60L*fL9r3L6DNVLwP5kX30)C+*&&icbgDpC&^SBMkvF)FphohBLc3)p%Q2)1iDpOy)ri@ANL^YTrf~XmeDWTGxqW3UGiD2rwejnLzh1uPFrYZ^IO9NW4&d5Q8a!y0kQGau9JBnwC*DJ7j5eTS*&_Nq5VQOcl6mHwh=$ZEXQv6WZLMoq$vy5)(O3ly(AM0=%sl&14Nr@l_Vsi1*Qx0Z!KQ*CgTT&!S+wUuujlPq2ZgJXkudcSO%W)$8@qvLr=yHPiyNmpBj(NxNUufVhLK@%HXypup@uVu#3AsirH8zX*eC~JJyYG4&O&e&(&ubzf!#AGvhfM4yCQ^uI6AEA~xrTBzcb@0pD4xBBGbD~OVQ1^AgxIOf+ZJukBlG2jY~y&4RGEZHb6d)M+bpSZ0g6V_SBNo*13Ph2E8IEnTG4#e%WFesCN~)t5PLMZeNnP4a)9#*AORMd*)+NwRR^7IiSgeN3E3hC%F)OmYHHWB_aluSel3avdUTv7&@G++DE16bhJQD+Ro=)p!*oo~32ACe$Oqi)7x5W3P=uhvxtA6g1eN1g)Ad=za$%^7D
+ 401251 <AdnFUdn
+ 401653 gijlk0!0	
+ 401688 ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ@
+ 401711 ~eoL|
+ 401772 5:aJV
+ 4017db j#2O.
+ 401802 nM2Q
+ 40181c Z-}>
+ 40182a )t-omg
